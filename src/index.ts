38525fd021ae02cdf120cbef8ecef2761cb6c8e7
ndricimjahaj <jahajndricim@gmail.com>
2025-08-24 14:31:10 +0200

    add fly/toml file

--- src/index.ts
+++ src/index.ts
@@ -25,5 +25,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/audit", auditRouter);
 app.use("/api/chronologies", chronologiesRoutes);
 
+app.get("/health", (req, res) => res.send("OK"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
